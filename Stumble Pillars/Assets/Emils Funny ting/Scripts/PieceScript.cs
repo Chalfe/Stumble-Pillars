0c6939aee1293696f3f018b6c222789a405c7ab1
Chalfe <kewin.paepke@gmail.com>
2022-11-16 12:46:47 +0100

    Made health and stuff

--- Stumble Pillars/Assets/Emils Funny ting/Scripts/PieceScript.cs
+++ Stumble Pillars/Assets/Emils Funny ting/Scripts/PieceScript.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PieceScript : MonoBehaviour
 {
     Rigidbody2D rb2D;
+    public GameObject PieceDestroyer;
+    private float minusOne = -1f;
+
+
     private void Start()
     {
         GameObject myGameObject = gameObject;
@@ -12,12 +17,14 @@ public class PieceScript : MonoBehaviour
         rb2D.gravityScale = 0f;
         rb2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
         gameObject.AddComponent(typeof(PieceMovement));
+        PieceDestroyer = GameObject.FindGameObjectWithTag("DeadZone");
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "DeadZone")
         {
             Destroy(gameObject);
+            PieceDestroyer.GetComponent<Health>().health = PieceDestroyer.GetComponent<Health>().health - 1; 
         }
         rb2D.gravityScale = 1f;
         Destroy(gameObject.GetComponent(typeof(PieceMovement)));
